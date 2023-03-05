2202d07b7e3294ad5ce485acaad5708863fc0896
Jakub Szwajka <szwajkajakub@gmail.com>
2023-03-05 20:20:33 +0100

    Add course detail view

--- course_tracker/services_application/course_services/course_view.py
+++ course_tracker/services_application/course_services/course_view.py
@@ -13,3 +13,12 @@ class CourseView(APIView):
         courses = service.list(user=request.user)
         serialized = [course.dict() for course in courses]
         return Response(serialized, status.HTTP_200_OK)
+
+
+class CourseDetailView(APIView):
+    def get(self, request, course_id, **kwargs):
+        service = CourseCatalogService()
+        course = service.retrieve(user=request.user, course_id=course_id)
+        if course:
+            return Response(course.dict(),status.HTTP_200_OK)
+        return Response(dict(), status.HTTP_404_NOT_FOUND)
--- course_tracker/services_application/urls.py
+++ course_tracker/services_application/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from course_tracker.services_application.course_services.course_view import CourseView
+from course_tracker.services_application.course_services.course_view import CourseDetailView, CourseView
 from course_tracker.services_application.course_services.enrollment_view import (
     CourseEnrollmentView,
 )
@@ -8,5 +8,6 @@ from course_tracker.services_application.course_services.enrollment_view import 
 
 urlpatterns = [
     path("course-catalog/", CourseView.as_view(), name="course-catalog"),
+    path("course-catalog/<int:course_id>", CourseDetailView.as_view(), name="course-detail"),
     path("enroll-course/", CourseEnrollmentView.as_view(), name="enroll-course"),
 ]
