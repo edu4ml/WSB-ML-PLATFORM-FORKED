35de9bd8a2e223f8814abd0049dda56b15dbd804
Jakub Szwajka <szwajkajakub@gmail.com>
2023-03-21 20:10:48 +0100

    Cypress config change

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
     },
 
     e2e: {
-        baseUrl: 'http://127.0.0.1:8000',
+        baseUrl: 'http://0.0.0.0:8000',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
