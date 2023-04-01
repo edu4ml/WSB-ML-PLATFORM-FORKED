97ec00a368d2e102c341eaa8302c266188cea836
Jakub Szwajka <szwajkajakub@gmail.com>
2023-04-01 18:26:07 +0200

    Add new texts structure - not implemented into codebase

--- frontend/src/texts.tsx
+++ frontend/src/texts.tsx
@@ -1,4 +1,3 @@
-export const TEXT_PUBLISH = 'Opublikuj';
 export const TEXT_COURSE_PUBLISHED = 'Kurs opublikowany!';
 export const TEXT_SOMETHING_WENT_WRONG = 'Ups! coś poszło nie tak!';
 export const TEXT_COURSE_SAVED = 'Kurs zapisany!';
@@ -55,3 +54,140 @@ export const TEXT_COURSE_PUBLISH_NO_STEP_OR_DESCRIPTION_WARNING =
     'Kurs nie zawiera żadnych kroków lub opisu! Dodaj, aby opublikować.';
 export const TEXT_COURSE_SAVE_NO_STEP_OR_DESCRIPTION_WARNING =
     'Kurs nie zawiera żadnych kroków ani opisu! Dodaj krok lub opis, aby zapisać.';
+
+// ---------------------------------- //
+
+// Button texts
+export const BTN_PUBLISH = 'Opublikuj';
+export const BTN_SAVE = 'Zapisz';
+export const BTN_ADD_STEP = 'Dodaj krok';
+export const BTN_CREATE_COURSE = 'Utwórz kurs';
+export const BTN_CREATE_COURSE_COMPONENT = 'Utwórz komponent';
+export const BTN_REMOVE = 'Usuń';
+
+// Titles
+export const TITLE_NEW_COURSE = 'Nowy kurs';
+export const TITLE_COURSES_ALL = 'Wszystkie kursy';
+export const TITLE_NEW_COURSE_COMPONENT = 'Nowy komponent';
+export const TITLE_EDIT_COURSE_COMPONENT = 'Edytuj';
+export const TITLE_COURSE_COMPONENTS_PAGE =
+    'Komponenty z których składa się kurs';
+export const TITLE_COURSE_PAGE = 'Twoje kursy';
+export const TITLE_EXTERNAL_RESOURCE_CREATE_NEW = 'Utwórz nowy';
+export const TITLE_COURSE_COMPONENT_EDIT_RESOURCES = 'Załączniki';
+
+// Notifications
+export const NOTIF_COURSE_PUBLISHED = 'Kurs opublikowany!';
+export const NOTIF_SOMETHING_WENT_WRONG = 'Ups! coś poszło nie tak!';
+export const NOTIF_COURSE_SAVED = 'Kurs zapisany!';
+export const NOTIF_COURSE_COMPONENT_CREATED = 'Komponent utworzony!';
+export const NOTIF_COURSE_COMPONENT_UPDATED = 'Komponent zaktualizowany!';
+export const NOTIF_COURSE_COMPONENT_DELETED = 'Komponent usunięty!';
+
+// Other texts
+export const TXT_DRAFT_VERSION = 'Wersja robocza';
+export const TXT_PUBLISHED_VERSION = 'Ten kurs jest opublikowany';
+export const TXT_EXERCISE = 'Ćwiczenie';
+export const TXT_EXERCISES = 'Ćwiczenia';
+export const TXT_VERIFICATION = 'Weryfikacja';
+export const TXT_AUTOMATIC = 'automatyczne';
+export const TXT_SEND_FILE = 'przesłanie pliku';
+export const TXT_MANUALLY = 'ręcznie';
+export const TXT_TITLE = 'Tytuł';
+export const TXT_DESCRIPTION = 'Opis';
+export const TXT_ADD_STEP_TO_COURSE = 'Dodaj krok do kursu';
+export const TXT_SUBMISSIONS = 'Przesłane zadania';
+export const TXT_EVALUATION = 'Ocena';
+export const TXT_EVALUATION_STATUS = 'Status oceny';
+export const TXT_NEW_COURSE_TITLE = 'Tytuł nowego kursu';
+export const TXT_COURSE_TITLE = 'Tytuł kursu';
+export const TXT_FORM_NO_TITLE_WARNING = 'Podaj tytuł nowego kursu!';
+export const TXT_FORM_NO_DESCRIPTION_WARNING = 'Dodaj opis do formularza!';
+export const TXT_FORM_SELECT_COMPONENT_TYPE_PLACEHOLDER =
+    'Wybierz typ komponentu';
+export const TXT_COURSE_COMPONENT_TYPE_FILE_EVALUATION = 'Zadanie z plikiem';
+export const TXT_COURSE_COMPONENT_TYPE_EXERCISE = 'Ćwiczenie';
+export const TXT_COURSE_COMPONENT_TYPE_UNKNOWN = 'Inne';
+export const TXT_403_DESCRIPTION = 'Ups, nie powinno Cię tu być!.';
+export const TXT_TESTS = 'Testy';
+export const TXT_TASKS = 'Zadania';
+export const TXT_NAME = 'Nazwa';
+export const TXT_TYPE = 'Typ';
+export const TXT_EXTERNAL_RESOURCE_TITLE = 'Tytuł';
+export const TXT_EXTERNAL_RESOURCE_NO_TITLE_WARNING = 'Podaj tytuł!';
+export const TXT_EXTERNAL_RESOURCE_URL = 'URL';
+export const TXT_EXTERNAL_RESOURCE_NO_URL_WARNING = 'Podaj URL!';
+export const TXT_COURSE_PUBLISH_NO_STEP_OR_DESCRIPTION_WARNING =
+    'Kurs nie zawiera żadnych kroków lub opisu! Dodaj, aby opublikować.';
+export const TXT_COURSE_SAVE_NO_STEP_OR_DESCRIPTION_WARNING =
+    'Kurs nie zawiera żadnych kroków ani opisu! Dodaj krok lub opis, aby zapisać.';
+
+// Categories
+export const CATEGORY_BUTTON_TEXTS = {
+    publish: BTN_PUBLISH,
+    save: BTN_SAVE,
+    addStep: BTN_ADD_STEP,
+    createCourse: BTN_CREATE_COURSE,
+    createCourseComponent: BTN_CREATE_COURSE_COMPONENT,
+    remove: BTN_REMOVE,
+};
+
+export const CATEGORY_TITLES = {
+    newCourse: TITLE_NEW_COURSE,
+    allCourses: TITLE_COURSES_ALL,
+    newCourseComponent: TITLE_NEW_COURSE_COMPONENT,
+    editCourseComponent: TITLE_EDIT_COURSE_COMPONENT,
+    courseComponentsPage: TITLE_COURSE_COMPONENTS_PAGE,
+    coursePage: TITLE_COURSE_PAGE,
+    externalResourceCreateNew: TITLE_EXTERNAL_RESOURCE_CREATE_NEW,
+    courseComponentEditResources: TITLE_COURSE_COMPONENT_EDIT_RESOURCES,
+};
+
+export const CATEGORY_NOTIFICATIONS = {
+    coursePublished: NOTIF_COURSE_PUBLISHED,
+    somethingWentWrong: NOTIF_SOMETHING_WENT_WRONG,
+    courseSaved: NOTIF_COURSE_SAVED,
+    courseComponentCreated: NOTIF_COURSE_COMPONENT_CREATED,
+    courseComponentUpdated: NOTIF_COURSE_COMPONENT_UPDATED,
+    courseComponentDeleted: NOTIF_COURSE_COMPONENT_DELETED,
+};
+
+export const CATEGORY_OTHER_TEXTS = {
+    draftVersion: TXT_DRAFT_VERSION,
+    publishedVersion: TXT_PUBLISHED_VERSION,
+    exercise: TXT_EXERCISE,
+    exercises: TXT_EXERCISES,
+    verification: TXT_VERIFICATION,
+    automatic: TXT_AUTOMATIC,
+    sendFile: TXT_SEND_FILE,
+    manually: TXT_MANUALLY,
+    title: TXT_TITLE,
+    description: TXT_DESCRIPTION,
+    addStepToCourse: TXT_ADD_STEP_TO_COURSE,
+    submissions: TXT_SUBMISSIONS,
+    evaluation: TXT_EVALUATION,
+    evaluationStatus: TXT_EVALUATION_STATUS,
+    newCourseTitle: TXT_NEW_COURSE_TITLE,
+    courseTitle: TXT_COURSE_TITLE,
+    formNoTitleWarning: TXT_FORM_NO_TITLE_WARNING,
+    formNoDescriptionWarning: TXT_FORM_NO_DESCRIPTION_WARNING,
+    formSelectComponentTypePlaceholder:
+        TXT_FORM_SELECT_COMPONENT_TYPE_PLACEHOLDER,
+    courseComponentTypeFileEvaluation:
+        TXT_COURSE_COMPONENT_TYPE_FILE_EVALUATION,
+    courseComponentTypeExercise: TXT_COURSE_COMPONENT_TYPE_EXERCISE,
+    courseComponentTypeUnknown: TXT_COURSE_COMPONENT_TYPE_UNKNOWN,
+    error403Description: TXT_403_DESCRIPTION,
+    tests: TXT_TESTS,
+    tasks: TXT_TASKS,
+    name: TXT_NAME,
+    type: TXT_TYPE,
+    externalResourceTitle: TXT_EXTERNAL_RESOURCE_TITLE,
+    externalResourceNoTitleWarning: TXT_EXTERNAL_RESOURCE_NO_TITLE_WARNING,
+    externalResourceUrl: TXT_EXTERNAL_RESOURCE_URL,
+    externalResourceNoUrlWarning: TXT_EXTERNAL_RESOURCE_NO_URL_WARNING,
+    coursePublishNoStepOrDescriptionWarning:
+        TXT_COURSE_PUBLISH_NO_STEP_OR_DESCRIPTION_WARNING,
+    courseSaveNoStepOrDescriptionWarning:
+        TXT_COURSE_SAVE_NO_STEP_OR_DESCRIPTION_WARNING,
+};
